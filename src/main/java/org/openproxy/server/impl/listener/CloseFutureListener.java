d951526dcb8dc2e317003636663913056368cc53
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2017-12-27 12:28:25 +0100

    Append logger to the listener

--- src/main/java/org/openproxy/server/impl/listener/CloseFutureListener.java
+++ src/main/java/org/openproxy/server/impl/listener/CloseFutureListener.java
@@ -8,6 +8,8 @@ package org.openproxy.server.impl.listener;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.EventLoop;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,10 +17,13 @@ import io.netty.channel.EventLoop;
  */
 public class CloseFutureListener implements ChannelFutureListener {
 
+	private static final Logger LOG = Logger.getLogger(CloseFutureListener.class.getName());
+
 	@Override
 	public void operationComplete(ChannelFuture f) throws Exception {
 		EventLoop eventLoop = f.channel().eventLoop();
 		eventLoop.shutdownGracefully();
+		LOG.log(Level.INFO, "Future " + f.toString() + " was completed");
 	}
-	
+
 }
